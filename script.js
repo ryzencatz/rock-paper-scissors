34de49f399cbe6946ff0c7a820f5e69855055fa2
ryzencatz <ryzencatz@gmail.com>
2026-05-17 10:14:18 -0700

    finished game

--- script.js
+++ script.js
@@ -11,14 +11,55 @@ const getComputerChoice = () => {
 }
 
 const getHumanChoice = () => {
-    choice = window.prompt("rock paper or scissors?");
-    while (!(choice === 'rock' | choice === "paper" | choice === 'scissors')) {
+    choice = window.prompt("rock paper or scissors?").toLowerCase();
+    while (!(choice === 'rock' || choice === "paper" || choice === 'scissors')) {
         console.log('invalid input');
-        choice = window.prompt("rock paper or scissors?");
+        choice = window.prompt("rock paper or scissors?").toLowerCase();
     }
     
     return (choice);
 }
 
-let humanScore = 0;
-let computerScore = 0;
+const playGame = () => {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    const playRound = (humanChoice, computerChoice) => {
+        if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            console.log("you win! rock beats scissors"); 
+            humanScore++;
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            console.log("you win! paper beats rock"); 
+            humanScore++;
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            console.log("you win! scissors beats paper"); 
+            humanScore++;
+        } else if (computerChoice === 'rock' && humanChoice === 'scissors') {
+            console.log("you lose! rock beats scissors"); 
+            computerScore++;
+        } else if (computerChoice === 'paper' && humanChoice === 'rock') {
+            console.log("you lose! paper beats rock"); 
+            computerScore++;
+        } else if (computerChoice === 'scissors' && humanChoice === 'paper') {
+            console.log("you lose! scissors beats paper"); 
+            computerScore++;
+        } else if (computerChoice === humanChoice) {
+            console.log("you tied!");
+        }
+    }
+
+    for (let i = 1; i < 6; i++) {
+        console.log(`this is round ${i}`);
+
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+        console.log(`your score is ${humanScore} and computer's is ${computerScore}`);
+    }
+
+
+}
+
+
+playGame();
